282b9ed451846e8275b577501215b05e088d89fb
jymeraz <jymeraz@miners.utep.edu>
2020-02-26 14:46:15 -0700

    Updated work towards updating the length in recursive search.

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -11,7 +11,6 @@ public class SnakeGame {
   }
 
   public SnakeGame(boolean[][] inputArray, int row, int col){
-    // ********* Why do i have to initialize the column size? It gives me an error if I dont. **********
     game = new boolean[inputArray.length][inputArray[0].length];
 
     for (int i = 0; i < inputArray.length; i++){
@@ -23,40 +22,40 @@ public class SnakeGame {
     headPosition = new int[2];
     headPosition[0] = row;
     headPosition[1] = col;
-
     tailPosition = new int[3];
   }
 
   public int[] findTailExhaustive() {
+    // Reset the counter to equal to zero before checking through the array.
     resetCounters();
     int length = 0;
-    //int[] tailPosition = new int[3];
     tailPosition[0] = -1;
     tailPosition[1] = -1;
     boolean foundTail = false;
 
-    // For when the tail is the head.
+    // Check if the tail and the head are the same value.
     if(game.length == 1 && game[0].length == 1){
+      // Iterate exhaustive checks.
       exhaustiveChecks++;
       tailPosition[0] = 0;
       tailPosition[1] = 0;
       tailPosition[2] = 1;
-      foundTail = true;
+      // Return the tail position array.
       return tailPosition;
     }
 
+    // Iterate through the array to find the tail.
     for(int i = 0; i < game.length; i++){
       for (int j = 0; j < game[i].length; j++) {
+        // Only iterate exhaustive checks if the tail is not found.
         if(!foundTail){
           exhaustiveChecks++;
         }
+        // If the snake exists, check if it is the tail and iterate the length.
         if (game[i][j]) {
           length++;
           if (!foundTail) {
-            // Check the (top, bottom, left, right) neighbors around to make sure that they are not
-            // true.
-            // Only one neighbor can be true.
-            // Check that the coordinates are not the head.
+            // Check that the position only has one neighbor and that it is not the head.
             if (i == 0 && j == 0) {
               if ((!game[i][j + 1] || !game[i + 1][j])
                   && (i != headPosition[0] || j != headPosition[1])) {
@@ -136,14 +135,18 @@ public class SnakeGame {
         }
       }
     }
+    // Set the second index in the tail position equal to the length.
     tailPosition[2] = length;
+    // Return the tail position array.
     return tailPosition;
   }
 
   public int[] findTailRecursive(){
-    tailPosition[2] = 0;
+    // Reset the counters.
     resetCounters();
-    //int[] tailPosition2 = new int[3];
+    tailPosition[2] = 0;
+
+    // Initialize two arrays with the same contents as headPosition to avoid overwriting to the headPosition array.
     int[] currentPosition = new int[2];
     int[] previousPosition = new int[2];
 
@@ -154,50 +157,35 @@ public class SnakeGame {
       previousPosition[i] = headPosition[i];
     }
 
-    //tailPosition[2]++;
-    for(int i = 0; i < tailPosition.length; i++){
-      tailPosition[i] = findTailRecursive(currentPosition, previousPosition)[i];
+    // Check if the tail and the head are the same value.
+    if(game.length == 1 && game[0].length == 1){
+      // Iterate exhaustive checks.
+      exhaustiveChecks++;
+      tailPosition[0] = 0;
+      tailPosition[1] = 0;
+      tailPosition[2] = 1;
+      // Return the tail position array.
+      return tailPosition;
     }
 
-    //tailPosition2 = findTailRecursive(headPosition, headPosition);
-    //recursiveChecks++;
-    //System.out.println("recursive checks: " + recursiveChecks);
-    //tailPosition2[2] = recursiveChecks;
-    return tailPosition;
+    // Return the array returned by the recursive method.
+    return findTailRecursive(currentPosition, previousPosition);
   }
 
-//  private int[] findTailRecursive(int[] currentPosition, int[] previousPosition)
-//  -- overloads the previous method, and is similar in definition,
-//  but starts at a position other than the head position (used for the recursive calls),
-//  also takes in the position of the previous body position (to exclude it from deciding the next position).
-//  Increments the recursiveChecks counter with each (x',y') cell that is examined.
-//  Hint: the call for starting from the head position made from the public method should be findTailRecursive(headPosition, headPosition).
-  // Change to private rn.
-
-  // Where do i put the recursive checks?
   private int[] findTailRecursive(int[] currentPosition, int[] previousPosition){
-    //FIXME length is always 2 units larger than the actual length
-    //System.out.println("length: " + tailPosition[2]);
-    //int[] tailPosition = new int[3];
-    tailPosition[2]++;
+    // Iterate recursive Checks and increase the length by 1.
     recursiveChecks++;
-    //int[] tailPosition = new int[3];
+    tailPosition[2]++;
     int rowCurrent = currentPosition[0];
     int colCurrent = currentPosition[1];
 
     int rowPrevious = previousPosition[0];
     int colPrevious = previousPosition[1];
 
-    // BASE CASE.
-    // IF THERE ARE NO OTHER ONES AROUND IT, then return it.
+    // Base case for recursive method.
+    // Check whether the position has one neighbor and is not the head.
     if (rowCurrent != headPosition[0] || colCurrent != headPosition[1]) {
       if (rowCurrent == 0 && colCurrent == 0) {
-        // If the one on the right is not equal to the previous and it's empty AND if the one on the
-        // bottom is not equal to the previous and its empty
-        // if it is equal to the previous and it's not empty, it's fine
-        // If the one on the right is equal to the previous AND the one on the bottom is empty *OR*
-        // if the one on the bottom is equal to the right and the bottom is empty
-        // Or, if one is true and one is not
         if (game[rowCurrent][colCurrent + 1] && !game[rowCurrent + 1][colCurrent]) {
           tailPosition[0] = rowCurrent;
           tailPosition[1] = colCurrent;
@@ -208,9 +196,6 @@ public class SnakeGame {
           tailPosition[1] = colCurrent;
           return tailPosition;
         }
-
-        // if the one on the left is the same as the previous and the one on the bottom is empty
-        // if the one on the bottom is the same as the previous and the left is empty.
       } else if (rowCurrent == 0 && colCurrent == game[rowCurrent].length - 1) {
         if (game[rowCurrent][colCurrent - 1] && !game[rowCurrent + 1][colCurrent]) {
           tailPosition[0] = rowCurrent;
@@ -222,9 +207,6 @@ public class SnakeGame {
           tailPosition[1] = colCurrent;
           return tailPosition;
         }
-        // Only one can be true.
-        // if one is true and the rest are false. s
-        // If one is equal to previous and the rest are empty
       } else if (rowCurrent == 0) {
         if (game[rowCurrent][colCurrent - 1]
             && !game[rowCurrent + 1][colCurrent]
@@ -247,7 +229,6 @@ public class SnakeGame {
           tailPosition[1] = colCurrent;
           return tailPosition;
         }
-        // top and right
       } else if (rowCurrent == game.length - 1 && colCurrent == 0) {
         if (game[rowCurrent + 1][colCurrent] && !game[rowCurrent][colCurrent + 1]) {
           tailPosition[0] = rowCurrent;
@@ -372,8 +353,8 @@ public class SnakeGame {
       }
     }
 
-    // RECURSIVE CALLS HERE
-    // Based on where the current position is, only check certain areas to avoid going out of bounds.
+    // Recursive calls if base case is not entered.
+    // Call the method again if the position next to the current position is not empty and is not the previous position.
     if(rowCurrent == 0 && colCurrent == 0){
       if (game[rowCurrent][colCurrent + 1] && (rowCurrent != rowPrevious || colCurrent + 1 != colPrevious)) {
         previousPosition[0] = currentPosition[0];
@@ -385,7 +366,6 @@ public class SnakeGame {
         previousPosition[0] = currentPosition[0];
         previousPosition[1] = currentPosition[1];
         currentPosition[0] = rowCurrent + 1;
-        //System.out.println("IN recur length: " + tailPosition[2]);
         return findTailRecursive(currentPosition, previousPosition);
       }
 
@@ -396,7 +376,7 @@ public class SnakeGame {
         currentPosition[1] = colCurrent - 1;
         return findTailRecursive(currentPosition, previousPosition);
       }
-      if(!game[rowCurrent + 1][colCurrent] && (rowCurrent + 1 != rowPrevious || colCurrent != colPrevious)){
+      if(game[rowCurrent + 1][colCurrent] && (rowCurrent + 1 != rowPrevious || colCurrent != colPrevious)){
         previousPosition[0] = currentPosition[0];
         previousPosition[1] = currentPosition[1];
         currentPosition[0] = rowCurrent + 1;
@@ -422,10 +402,10 @@ public class SnakeGame {
         return findTailRecursive(currentPosition, previousPosition);
       }
     } else if (rowCurrent == game.length - 1 && colCurrent == 0){
-      if (game[rowCurrent + 1][colCurrent] && (rowCurrent + 1 != rowPrevious || colCurrent != colPrevious)){
+      if (game[rowCurrent - 1][colCurrent] && (rowCurrent - 1 != rowPrevious || colCurrent != colPrevious)){
         previousPosition[0] = currentPosition[0];
         previousPosition[1] = currentPosition[1];
-        currentPosition[0] = rowCurrent + 1;
+        currentPosition[0] = rowCurrent - 1;
         return findTailRecursive(currentPosition, previousPosition);
       }
       if(game[rowCurrent][colCurrent + 1] && (rowCurrent != rowPrevious || colCurrent + 1 != colPrevious)){
@@ -530,14 +510,18 @@ public class SnakeGame {
         return findTailRecursive(currentPosition, previousPosition);
       }
     }
-    // check if there is a cell that is not the previous, and make that the current.
-    // based on where it is, check all the sides and call recursively from there.
+
+    // Call is never executed.
     return tailPosition;
   }
+
+  // Reset the counters to avoid previous count from being taken into consideration.
   private void resetCounters(){
     exhaustiveChecks = 0;
     recursiveChecks = 0;
   }
+
+  // Create private methods for recursive and exhaustive checks.
   private static int getRecursiveChecks(){
     return recursiveChecks;
   }
@@ -545,6 +529,7 @@ public class SnakeGame {
     return exhaustiveChecks;
   }
 
+  // Create public methods for checks in order to access them for testing.
   public static int getRecursiveChecksPublic(){
     return recursiveChecks;
   }
--- java/src/SnakeGameTester.java
+++ java/src/SnakeGameTester.java
@@ -1,14 +1,12 @@
-//import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-// ******************* what kinds of tests? do we test how many checks they made in each too?
-// ******************** Do some check the number of recursive/exhaustive checks?
-
 public class SnakeGameTester {
-  // This is a test to check that the exhaustive test can perform its basic functions.
-  // A simple linear snake is used to avoid any confounding error in the code.
+  // This is a test to check that the exhaustive test can perform its basic functions and returns the correct array.
+  // This array includes the row, column, and length.
+  // The true values are arranged in a line in order to avoid any error that could arise if the snake was in varying shapes.
+  // The true values were surrounded by false's in order to test the if-statement that does not include the edge of the array.
   // This test passed, so the exhaustive method works in finding the tail when the snake is a straight line.
   @Test
   public void testFindTailExhaustive1() {
@@ -31,8 +29,11 @@ public class SnakeGameTester {
     }
   }
 
-  // This test is to check of the exhaustive method works in arrays where the snake is not just in a straight line.
-  // This test passed, so the exhaustive method works in finding the tail with varying shapes.
+  // This test is to check of the exhaustive method works in returning the correct row value.
+  // This is important because it makes sure that the tail is actually being found.
+  // The snake was arranged in a shape other than straight in order to make sure that the program can handle varying shapes.
+  // The head, as well as the tail, were placed on the right edge of the array in order to check if the boundaries are well-checked in the program.
+  // This test passed, so the exhaustive method works in finding the row value.
   @Test
   public void testFindTailExhaustive2() {
     boolean[][] inputArray = {
@@ -42,21 +43,17 @@ public class SnakeGameTester {
         {false, true, true, true},
         {false, false, false, true}};
 
-    int[] expectedArray = {4, 3, 9};
-
     SnakeGame testTwo = new SnakeGame(inputArray, 0, 3);
-
-//    for (int i = 0; i < expectedArray.length; i++) {
-//      System.out.print(testTwo.findTailExhaustive()[i] + " ");
-//    }
-
-    for (int i = 0; i < expectedArray.length; i++) {
-      assertEquals(expectedArray[i], testTwo.findTailExhaustive()[i]);
-    }
+    assertEquals(4, testTwo.findTailExhaustive()[0]);
   }
 
-  // This test is to check of the exhaustive method works in arrays of differing sizes.
-  // This test passed, so the exhaustive method works in finding the tail with arrays of varying sizes.
+  // This test is to check of the exhaustive method works in returning the correct column value.
+  // This test is important in order to make sure that the tail is successfully being located.
+  // This test uses a bigger array in order to make sure that the program can handle different sized arrays.
+  // The head was placed in the center of the array and the tail was placed at the edge of the array.
+  // The tail was placed on the left edge in order to make sure that the program handles values in the left edge correctly.
+  // This is in order to check if the method can handle both of them entering different if-statements referring to their boundaries.
+  // This test passed, so the exhaustive method works in finding the column value of the tail.
   @Test
   public void testFindTailExhaustive3() {
     boolean[][] inputArray = {
@@ -70,41 +67,38 @@ public class SnakeGameTester {
         {false, false, false, false, false, false, false, false},
         {false, false, false, false, false, false, false, false}};
 
-    int[] expectedArray = {3, 0, 17};
-
     SnakeGame testThree = new SnakeGame(inputArray, 2, 7);
-
-//        for (int i = 0; i < expectedArray.length; i++) {
-//      System.out.print(testThree.findTailExhaustive()[i] + " ");
-//    }
-
-    for (int i = 0; i < expectedArray.length; i++) {
-      assertEquals(expectedArray[i], testThree.findTailExhaustive()[i]);
-    }
+    assertEquals(0, testThree.findTailExhaustive()[1]);
   }
 
-  // This test is to check of the exhaustive method works in the smallest possible size.
-  // This test failed at first because the code made sure that the head position was not the tail position.
-  // This was fixed by adding a special case scenario.
+  // This test is to check of the exhaustive method works in returning the correct length of the snake.
+  // This is important because the program should continue iterating even after the tail is found in order to find the length.
+  // This test case uses the same array as the previous test in order to make sure that the method can find both the tail and the length.
+  // This is to avoid the case where the method only works with certain arrays.
+  // This test case makes sure that this mechanism is actually taking place.
+  // This test passed, so the exhaustive method works in finding the length of the snake.
   @Test
   public void testFindTailExhaustive4() {
     boolean[][] inputArray = {
-        {true}};
+        {false, false, false, false, false, false, false, false},
+        {false, false, false, false, false, false, false, false},
+        {false, true, true, true, false, false, true, true},
+        {true, true, false, true, false, false, true, false},
+        {false, false, false, true, true, false, true, false},
+        {false, false, false, false, true, false, true, false},
+        {false, false, false, false, true, true, true, false},
+        {false, false, false, false, false, false, false, false},
+        {false, false, false, false, false, false, false, false}};
 
-    int[] expectedArray = {0, 0, 1};
+    SnakeGame testFour = new SnakeGame(inputArray, 2, 7);
 
-    SnakeGame testFour = new SnakeGame(inputArray, 0, 0);
-
-//    for (int i = 0; i < expectedArray.length; i++) {
-//      System.out.print(testFour.findTailExhaustive()[i] + " ");
-//    }
-
-    for (int i = 0; i < expectedArray.length; i++) {
-      assertEquals(expectedArray[i], testFour.findTailExhaustive()[i]);
-    }
+    assertEquals(17, testFour.findTailExhaustive()[2]);
   }
 
   // This test is to check that the checks for the exhaustive method output the correct value.
+  // This test case is important because it is important to make sure that the checks do not iterate once the tail is found.
+  // The head, as well as the tail, were placed on edge positions because those are special locations checked in order to avoid going out of bounds.
+  // This makes sure that the top left corner and right edge are being handled correctly.
   // This test failed at first because my code only iterated exhaustive checks if the value at the indexes was true.
   // This error was fixed and the test passed by iterating if the tail was not found.
   @Test
@@ -120,13 +114,37 @@ public class SnakeGameTester {
     assertEquals(9, SnakeGame.getExhaustiveChecksPublic());
   }
 
+  // This test is to check of the exhaustive method works in the smallest possible size.
+  // This test is important because there needs to be a mechanism to cover this case.
+  // This test failed at first because the code made sure that the head position was not the tail position.
+  // This was fixed by adding a special case scenario.
+  @Test
+  public void testFindTailExhaustive6() {
+    boolean[][] inputArray = {
+        {true}};
+
+    int[] expectedArray = {0, 0, 1};
+
+    SnakeGame testSix = new SnakeGame(inputArray, 0, 0);
+
+    for (int i = 0; i < expectedArray.length; i++) {
+      assertEquals(expectedArray[i], testSix.findTailExhaustive()[i]);
+    }
+  }
+
+  // This test is to make sure that the recursive method can correctly find the tail and length of a simple given array.
+  // This test is important because it is important to make sure that the returned array matches what is expected.
+  // The array is placed on the top left in order to make sure that the top left corner and the left edge is being handled correctly.
+  // This test did not pass at first because it was incrementing the length and recursive checks too many times.
+  // This was fixed by altering the public recursive checks method and returning the array returned by the private method directly rather than copying it and then returning it.
+  // This test was successful, so the recursive method works at finding the tail indexes.
   @Test
   public void testFindTailRecursive1() {
     boolean[][] inputArray = {
         {true, false},
         {true, false},
         {true, false},
-        {true, false},
+        {false, false},
         {false, false}};
 
     int[] expectedArray = {2, 0, 3};
@@ -134,36 +152,115 @@ public class SnakeGameTester {
 
     SnakeGame testOne = new SnakeGame(inputArray, 0, 0);
 
-
-    for (int i = 0; i < expectedArray.length; i++) {
-      System.out.print(testOne.findTailRecursive()[i] + " ");
-    }
-//    for (int i = 0; i < testOne.findTailExhaustive().length; i++) {
-//      assertEquals(expectedArray[i], testOne.findTailRecursive()[i]);
-//    }
-  }
-
-  /*
-  // This test is to make sure that
-  @Test
-  public void testFindTailExhaustive3() {
-    boolean[][] inputArray = {
-        {true, true, true, true, true},
-        {true, true, true, true, true},
-        {true, true, true, true, true},
-        {true, true, true, true, true},
-        {true, true, true, true, true}};
-
-    int[] expectedArray = {4, 3, 9};
-
-    SnakeGame testOne = new SnakeGame(inputArray, 0, 3);
-
-    for (int i = 0; i < expectedArray.length; i++) {
-      System.out.print(testOne.findTailExhaustive()[i] + " ");
-    }
+    testOne.findTailRecursive();
 
     for (int i = 0; i < testOne.findTailExhaustive().length; i++) {
-      assertEquals(expectedArray[i], testOne.findTailExhaustive()[i]);
+      assertEquals(expectedArray[i], testOne.findTailRecursive()[i]);
     }
-  } */
+  }
+
+  // This test is to make sure that the recursive method works in finding the row value of the tail.
+  // This test is important because it is necessary to know if the tail is being found successfully.
+  // The tail was placed in the bottom left corner, which makes sure that the left edge and bottom edge are being handled correctly.
+  // This will test that it is correctly iterating through the whole array and not just one row/column.
+  // This test was successful, so varying shapes of the snake are also being handled correctly and the row value is correctly found.
+  @Test
+  public void testFindTailRecursive2() {
+    boolean[][] inputArray = {
+        {true, false, false, false, false, false},
+        {true, false, false, false, false, true},
+        {true, true, true, true, false, true},
+        {true, false, false, true, true, true},
+        {true, false, false, false, false, false}};
+
+    SnakeGame testTwo = new SnakeGame(inputArray, 1, 5);
+
+    assertEquals(0, testTwo.findTailRecursive()[0]);
+
+  }
+
+  // This test is to make sure that the column value is returned successfully by the recursive method.
+  // This test is important because it is important to know that the tail is being successfully found.
+  // This test uses a different sized array in order to make sure that it can be handled by the program.
+  // The head was placed on the left edge and the snake zig-zags throughout the array, which helps check all edges except for the top.
+  // These edges are all separate if-statements, so it is important to know that they are each being executed appropriately.
+  // This test was successful, which means that the column value is successfully found by the recursive method.
+  @Test
+  public void testFindTailRecursive3() {
+    boolean[][] inputArray = {
+        {false, false, false},
+        {true, false, false},
+        {true, true, false},
+        {false, true, false},
+        {true, true, false},
+        {true, false, false},
+        {true, true, true},
+        {false, false, true},
+        {false, false, true},
+        {true, true, true},
+        {true, false, false},
+        {true, true, false},
+        {false, true, false}};
+
+    SnakeGame testThree = new SnakeGame(inputArray, 1, 0);
+
+    assertEquals(1, testThree.findTailRecursive()[1]);
+  }
+
+  // ***************************
+  // This test is to make sure that the recursive method is returning the correct length of the snake.
+  // This test is important because it is important to know that the length is iterating correctly for each cell that is examined.
+  // This test was not working at first, but it was fixed by deleting an extra call in the public recursive method.
+  @Test
+  public void testFindTailRecursive5() {
+    boolean[][] inputArray = {
+        {false, true, false, false},
+        {false, true, false, false},
+        {false, true, true, false},
+        {false, false, true, false}};
+
+    SnakeGame testFive = new SnakeGame(inputArray, 0, 1);
+
+    assertEquals(5, testFive.findTailRecursive()[2]);
+  }
+
+  // This test is to make sure that the recursive checks are iterating as expected.
+  // This test is important because the checks should be iterating for every cell checked and not for additional or less times.
+  // This test failed at first due to an over-count, which was fixed by removing an extra call to the method.
+  // This test ended up being successful after this extra called was removed.
+
+  @Test
+  public void testFindTailRecursive4() {
+    boolean[][] inputArray = {
+        {false, true, false, false},
+        {false, true, false, false},
+        {false, true, true, false},
+        {false, false, true, false}};
+
+    SnakeGame testFour = new SnakeGame(inputArray, 0, 1);
+
+    testFour.findTailRecursive();
+
+    assertEquals(5, SnakeGame.getRecursiveChecksPublic());
+  }
+
+  // This test is to make sure that the smallest possible array is being taken into account.
+  // This test is important because it is good for the program to take into consideration all possible arrays.
+  // This test was at first unsuccessful due to an out-of-bounds exception, which was fixed by creating a separate if-conditional.
+  // This conditional was placed in the public recursive method in order to avoid that step being executed in each recursive call.
+
+  @Test
+  public void testFindTailRecursive6() {
+    boolean[][] inputArray = {
+        {true}};
+
+    int[] expectedArray = {0, 0, 1};
+    int[] a = {0, 0};
+
+    SnakeGame testSix = new SnakeGame(inputArray, 1, 0);
+
+    for (int i = 0; i < testSix.findTailExhaustive().length; i++) {
+      assertEquals(expectedArray[i], testSix.findTailRecursive()[i]);
+    }
+  }
 }
